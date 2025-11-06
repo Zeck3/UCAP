d611f7a20b3ad1f49eea277de04c85587eedfe80
Zeck3 <owmsowmsowms@gmail.com>
2025-11-06 22:32:58 +0800

    some code

--- frontend/src/api/userDepartmentApi.ts
+++ frontend/src/api/userDepartmentApi.ts
@@ -3,6 +3,5 @@ import type { UserDepartment } from "../types/userDepartmentTypes";
 
 export const fetchUserDepartment = async (departmentId: number): Promise<UserDepartment> => {
   const response = await axiosClient.get(`user/${departmentId}`);
-  console.log(response.data);
   return response.data;
 }
