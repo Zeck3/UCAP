531661d896c09df03cb4ab63d1bde215fa236620
Zeck3 <owmsowmsowms@gmail.com>
2025-11-21 12:28:02 +0800

    some code

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = os.environ.get("SECRET_KEY", "dummy")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
