70728702639e5dcc505ad5fa5423a50f6d398cc5
Zeck3 <owmsowmsowms@gmail.com>
2025-11-21 12:23:50 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,28 +1,16 @@
-import axios, { AxiosError, AxiosHeaders, type InternalAxiosRequestConfig } from "axios";
+import axios, { AxiosError } from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-});
-
-axiosClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  const method = (config.method ?? "get").toLowerCase();
-  const headers = AxiosHeaders.from(config.headers);
-
-  if (["post", "put", "patch", "delete"].includes(method)) {
-    headers.set("Content-Type", "application/json");
-  } else {
-    headers.delete("Content-Type");
-  }
-
-  config.headers = headers;
-  return config;
+  headers: { "Content-Type": "application/json" },
 });
 
 axiosClient.defaults.withCredentials = true;
 axiosClient.defaults.withXSRFToken = true;
 axiosClient.defaults.xsrfCookieName = "csrftoken";
 axiosClient.defaults.xsrfHeaderName = "X-CSRFToken";
+
 
 axiosClient.interceptors.response.use(
   (response) => response,
@@ -38,7 +26,10 @@ axiosClient.interceptors.response.use(
   (response) => response,
   (error) => {
     const axiosError = error as AxiosError;
-    if (axiosError.response?.status === 403 && window.location.pathname !== "/login") {
+    if (
+      axiosError.response?.status === 403 &&
+      window.location.pathname !== "/login"
+    ) {
       window.location.href = "/login";
     }
     return Promise.reject(error);
