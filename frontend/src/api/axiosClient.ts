448a647a5d3cf1665c64023f81426e97b6eb87a5
Zeck3 <owmsowmsowms@gmail.com>
2025-11-23 09:23:43 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -9,28 +9,28 @@ const axiosClient = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (axios.isAxiosError(error) && error.response?.status === 400) {
-      return Promise.reject(error);
-    }
-    return Promise.reject(error);
-  }
-);
+// axiosClient.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (axios.isAxiosError(error) && error.response?.status === 400) {
+//       return Promise.reject(error);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const axiosError = error as AxiosError;
-    if (
-      axiosError.response?.status === 403 &&
-      window.location.pathname !== "/login"
-    ) {
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+// axiosClient.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     const axiosError = error as AxiosError;
+//     if (
+//       axiosError.response?.status === 403 &&
+//       window.location.pathname !== "/login"
+//     ) {
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosClient;
