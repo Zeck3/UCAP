64642b2ad29a65537b00244cf712e12a3c7c3954
Zeck3 <owmsowmsowms@gmail.com>
2025-11-23 08:34:15 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,38 +1,24 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
 });
 
-axiosClient.defaults.withCredentials = true;
 axiosClient.defaults.withXSRFToken = true;
 axiosClient.defaults.xsrfCookieName = "csrftoken";
 axiosClient.defaults.xsrfHeaderName = "X-CSRFToken";
 
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (axios.isAxiosError(error) && error.response?.status === 400) {
-      return Promise.reject(error);
-    }
-    return Promise.reject(error);
-  }
-);
+axiosClient.interceptors.request.use((config) => {
+  const method = (config.method ?? "get").toLowerCase();
 
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const axiosError = error as AxiosError;
-    if (
-      axiosError.response?.status === 403 &&
-      window.location.pathname !== "/login"
-    ) {
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
+  if (["post", "put", "patch", "delete"].includes(method)) {
+    config.headers["Content-Type"] = "application/json";
+  } else {
+    delete config.headers["Content-Type"];
   }
-);
+
+  return config;
+});
 
 export default axiosClient;
