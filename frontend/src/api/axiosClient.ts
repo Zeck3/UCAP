af3f984fc884c95a946349d7ecb17176f8cd0171
Zeck3 <owmsowmsowms@gmail.com>
2025-11-23 13:10:08 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -8,28 +8,28 @@ const axiosClient = axios.create({
   xsrfHeaderName: "X-CSRFToken",
 });
 
-// axiosClient.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (axios.isAxiosError(error) && error.response?.status === 400) {
-//       return Promise.reject(error);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (axios.isAxiosError(error) && error.response?.status === 400) {
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  }
+);
 
-// axiosClient.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     const axiosError = error as AxiosError;
-//     if (
-//       axiosError.response?.status === 403 &&
-//       window.location.pathname !== "/login"
-//     ) {
-//       window.location.href = "/login";
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axiosClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const axiosError = error as AxiosError;
+    if (
+      axiosError.response?.status === 403 &&
+      window.location.pathname !== "/login"
+    ) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosClient;
