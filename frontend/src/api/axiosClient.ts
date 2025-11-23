e148d941b370e09366f03d9f26557fd6d84de33d
Zeck3 <owmsowmsowms@gmail.com>
2025-11-23 10:28:01 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError, type InternalAxiosRequestConfig } from "axios";
+import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -9,56 +9,28 @@ const axiosClient = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-function getCookie(name = "csrftoken"): string | null {
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                return decodeURIComponent(cookie.substring(name.length + 1));
-            }
-        }
-    }
-    return null;
-}
+// axiosClient.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (axios.isAxiosError(error) && error.response?.status === 400) {
+//       return Promise.reject(error);
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
-axiosClient.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-    const method = config.method ? config.method.toUpperCase() : '';
-    const isSafeMethod = ['GET', 'HEAD', 'OPTIONS', 'TRACE'].includes(method);
-    if (!isSafeMethod) {
-        const csrftoken = getCookie("csrftoken");
-      
-        if (csrftoken) {
-            config.headers['X-CSRFToken'] = csrftoken;
-        }
-    }
-    return config;
-}, error => {
-    return Promise.reject(error);
-});
-
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (axios.isAxiosError(error) && error.response?.status === 400) {
-      return Promise.reject(error);
-    }
-    return Promise.reject(error);
-  }
-);
-
-axiosClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    const axiosError = error as AxiosError;
-    if (
-      axiosError.response?.status === 403 &&
-      window.location.pathname !== "/login"
-    ) {
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+// axiosClient.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     const axiosError = error as AxiosError;
+//     if (
+//       axiosError.response?.status === 403 &&
+//       window.location.pathname !== "/login"
+//     ) {
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosClient;
