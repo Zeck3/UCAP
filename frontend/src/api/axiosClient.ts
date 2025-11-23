83b0b2ca49494f67cd90f945b6892a46ad46a144
Zeck3 <owmsowmsowms@gmail.com>
2025-11-23 08:46:28 +0800

    some code

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
   withXSRFToken: true,
   xsrfCookieName: "csrftoken",
   xsrfHeaderName: "X-CSRFToken",
