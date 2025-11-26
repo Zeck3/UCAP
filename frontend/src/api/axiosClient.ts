1172176783a4383e8f7386e1690d2fb265aa3d38
Zeck3 <owmsowmsowms@gmail.com>
2025-11-26 11:41:22 +0800

    /api/ setup

--- frontend/src/api/axiosClient.ts
+++ frontend/src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "/api/",
   withCredentials: true,
   withXSRFToken: true,
   xsrfCookieName: "csrftoken",
